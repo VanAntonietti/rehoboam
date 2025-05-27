8596265fb5a277be15d22d113a80c7092f2e7046
Van Antonietti <v.antonietti@icloud.com>
2025-05-27 00:20:29 -0300

    Design made and a few design tests made, code is broken in this commit need to implement the canvas divisory.

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,35 @@ use ratatui::{
     DefaultTerminal, Frame,
     buffer::Buffer,
     layout::Rect,
-    style::Stylize,
+    style::{Color, Stylize},
     symbols::border,
     text::Line,
-    widgets::{Block, Paragraph, Widget},
+    widgets::{
+        Block, Paragraph, Widget,
+        canvas::{Canvas, Circle},
+    },
 };
 use std::io;
 
 #[derive(Debug, Default)]
 pub struct App {
+    ball: Circle,
     exit: bool,
 }
 
 impl App {
+    const fn new() -> Self {
+        Self {
+            exit: false,
+            ball: Circle {
+                x: 80.00,
+                y: 80.00,
+                radius: 40.00,
+                color: Color::LightBlue,
+            },
+        }
+    }
+
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
@@ -25,7 +41,8 @@ impl App {
     }
 
     fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
+        //frame.render_widget(self, frame.area());
+        frame.render_widget(self.draw_ball(), frame.area());
     }
 
     fn handle_events(&mut self) -> io::Result<()> {
@@ -47,9 +64,15 @@ impl App {
     fn exit(&mut self) {
         self.exit = true;
     }
+
+    fn draw_ball(&self) -> impl Widget + '_ {
+        Canvas::default().paint(|ctx| {
+            ctx.draw(&self.ball);
+        })
+    }
 }
 
-impl Widget for &App {
+/* impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Line::from(" Rehoboam divergent circle ".bold());
         let block = Block::bordered()
@@ -60,11 +83,11 @@ impl Widget for &App {
             .block(block)
             .render(area, buf);
     }
-}
+}*/
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
-    let app_result = App::default().run(&mut terminal);
+    let app_result = App::new().run(&mut terminal);
     ratatui::restore();
     app_result
 }
