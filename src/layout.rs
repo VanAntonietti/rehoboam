d17ea26ebbb0482d5a09844e9599c98fe854b436
Van Antonietti <v.antonietti@icloud.com>
2025-05-30 09:20:50 -0300

    layout placement working

--- src/layout.rs
+++ src/layout.rs
@@ -1,15 +1,24 @@
 use crate::modules::github::github_draw;
-use ratatui::Frame;
 use ratatui::layout::{Constraint, Layout, Rect};
+use ratatui::{Frame, layout};
 
 pub fn draw_layout(frame: &mut Frame, area: Rect) {
-    let chunk = Layout::vertical([
-        Constraint::Length(9),
-        Constraint::Min(8),
-        Constraint::Length(7),
-    ])
-    .split(area);
-    github_draw(frame, chunk[0]);
-    github_draw(frame, chunk[1]);
-    github_draw(frame, chunk[2]);
+    let chunk = Layout::vertical([Constraint::Percentage(75), Constraint::Fill(1)]).split(area);
+    let bottom = Layout::default()
+        .direction(layout::Direction::Horizontal)
+        .constraints(vec![
+            Constraint::Percentage(33),
+            Constraint::Percentage(33),
+            Constraint::Fill(1),
+        ])
+        .split(chunk[1]);
+    let top = Layout::default()
+        .direction(layout::Direction::Horizontal)
+        .constraints(vec![Constraint::Percentage(75), Constraint::Fill(1)])
+        .split(chunk[0]);
+    github_draw(frame, top[0]);
+    github_draw(frame, top[1]);
+    github_draw(frame, bottom[0]);
+    github_draw(frame, bottom[1]);
+    github_draw(frame, bottom[2]);
 }
